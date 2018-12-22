b70292810de5fe40211942ac1fdcf197b6376ff6
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-12-22 18:47:58 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import dateparser
+from dateutil import parser
+from datetime import datetime
+
+app = Flask(__name__)
+
+def getabsolute(time):
+    gettime = str(dateparser.parse(time))
+    absolutetime = str(parser.parse(gettime).timestamp()).split('.')[0]
+    return absolutetime
+
+def getrelative(time):
+    getnow = str(datetime.now().timestamp()).split('.')[0]
+    getrelative = int(getabsolute(time)) - int(getnow)
+    return getrelative
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    input = request.args.get('input')
+    if input != '':
+        return '{}, {}'.format(getabsolute(input), getrelative(input))
+    else:
+        return 'Input required'
+
+
+
+if __name__ == '__main__':
+    app.run()
