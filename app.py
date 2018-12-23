af5c2e3f8653f4d5b555eb35ad337fb3b0a73a60
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-12-23 12:13:36 +0000

    try to force future dates only but fail

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 def getabsolute(time):
-    gettime = str(dateparser.parse(time))
+    gettime = str(dateparser.parse(time, settings={'PREFER_DATES_FROM': 'future'}))
     absolutetime = str(parser.parse(gettime).timestamp()).split('.')[0]
     return absolutetime
 
@@ -17,9 +17,12 @@ def getrelative(time):
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    input = request.args.get('input')
-    if input != '':
-        return '{}, {}'.format(getabsolute(input), getrelative(input))
+    if 'input' in request.args:
+        input = request.args.get('input')
+        if input != '':
+            return '{}, {}'.format(getabsolute(input), getrelative(input))
+        else:
+            return 'Input required'
     else:
         return 'Input required'
 
