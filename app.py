9dac0db66090e2c480a40815ed02d7e5d9ccb7cf
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-12-26 16:15:58 +0000

    attempts were made

--- app.py
+++ app.py
@@ -5,28 +5,30 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# def ensure_future_tense(text):
+#     if text.endswith("ago"):
+#         return
+#     return "in {}".format(text)
+
 def getabsolute(time):
     gettime = str(dateparser.parse(time, settings={'PREFER_DATES_FROM': 'future'}))
     absolutetime = str(parser.parse(gettime).timestamp()).split('.')[0]
     return absolutetime
 
-def getrelative(time):
-    getnow = str(datetime.now().timestamp()).split('.')[0]
+def getrelative(time, now):
+    getnow = str(now.timestamp()).split('.')[0]
     getrelative = int(getabsolute(time)) - int(getnow)
     return getrelative
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if 'input' in request.args:
         input = request.args.get('input')
         if input != '':
-            return '{}, {}'.format(getabsolute(input), getrelative(input))
+            return '{}, {}'.format(getabsolute(input), getrelative(input, datetime.now()))
         else:
             return 'Input required'
     else:
         return 'Input required'
-
-
 
 if __name__ == '__main__':
     app.run()
